2c06c5161b10b35cac1435a1c6e6de4629885261
michael <sheetsmj@msoe.edu>
2018-01-27 08:32:03 -0600

    Login/Registration is starting to look the part

--- Augmentr/ClientApp/app/app.module.shared.ts
+++ Augmentr/ClientApp/app/app.module.shared.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
@@ -15,6 +16,7 @@ import { CounterComponent } from './components/counter/counter.component';
         AppComponent,
         NavMenuComponent,
         CounterComponent,
+        LoginComponent,
         FetchDataComponent,
         HomeComponent
     ],
@@ -26,6 +28,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
+            { path: 'login', component: LoginComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
