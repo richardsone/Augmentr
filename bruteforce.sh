8bc192296a07f49931b689d37b029f4a04e92a5e
michael <sheetsmj@msoe.edu>
2018-02-13 06:39:38 -0600

    lol

--- bruteforce.sh
+++ bruteforce.sh
@@ -3,7 +3,7 @@ echo "================Starting CURL Requests================"
 while [ $counter -le 1000 ];
 do
     echo "Making request #$counter"
-    curl -d '{"Email":"fakeasdfsdAF'$counter'@mrstealurgurl.com", "Password":"lol'$counter'", "name":"JimBob'$counter'"}' -H "Content-Type: application/json" -v "localhost:12000/api/v1/auth/register"
+    curl -d '{"Email":"fakeAF'$counter'@mrstealurgurl.com", "Password":"lol'$counter'", "name":"JimBob'$counter'"}' -H "Content-Type: application/json" -v "localhost:12000/api/v1/auth/register"
     (( counter++ ))
     sleep 0.002
 done
